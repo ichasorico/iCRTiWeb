8e7d5b2bbcc413ae32fb559242ac3c5691780546
Íñigo Chaso <ichasorico@icrti.es>
2016-08-05 19:42:35 +0200

    Privera versión completa proyecto Web iCRTi. Cambios ejecución Test v7

--- src/main/java/utils/conexion.java
+++ src/main/java/utils/conexion.java
@@ -1,17 +1,8 @@
 package utils;
 
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
-
-import utils.BCrypt;
 
 public class conexion {
 
